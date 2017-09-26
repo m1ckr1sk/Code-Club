6c4c0195a67b1bc34344a8cc55d7206b52aaacdd
MButt <mjbutt@hotmail.co.uk>
2017-09-26 14:28:29 +0100

    Added project 1 into python directory

---
+++ python/about_me/main.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+print('Hi, I can code in Python!')
+
+print('''
+My favourite animals are dogs
+
+ o-###-
+   | |   #
+
+I live in Southampton
+
+   _|_
+  |   |
+  |#  |____        __
+  |   |    |      |# |
+  |  #|  # |   ___|  |_______
+ _|___|_#__|___\_^__________/
+               
+''')
+
+born = input('What year were you born?')
+born = int(born)
+age = 2025 - born
+print('In the year 2025 you\'ll be', age, 'years old!')
