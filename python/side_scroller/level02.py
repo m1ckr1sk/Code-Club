46a200294f8c83049c11c44d0018298680859c6a
mike <mike@dotnetbox>
2017-09-26 19:56:53 +0000

    Refactored the side scroller to make it easier to work with

---
+++ python/side_scroller/level02.py
@@ -0,0 +1,31 @@
+"""This module does blah blah."""
+from platform import Platform
+from level import Level
+
+
+# Create platforms for the level
+class Level02(Level):
+    """ Definition for level 2. """
+
+    def __init__(self, player):
+        """ Create level 1. """
+
+        # Call the parent constructor
+        Level.__init__(self, player)
+
+        self.level_limit = -1000
+
+        # Array with type of platform, and x, y location of the platform.
+        level = [[210, 30, 450, 570],
+                 [210, 30, 850, 420],
+                 [210, 30, 1000, 520],
+                 [210, 30, 1120, 280],
+                ]
+
+        # Go through the array above and add platforms
+        for platform in level:
+            block = Platform(platform[0], platform[1])
+            block.rect.x = platform[2]
+            block.rect.y = platform[3]
+            block.player = self.player
+            self.platform_list.add(block)
